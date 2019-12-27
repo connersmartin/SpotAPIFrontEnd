9562998335e0c8806b95b8779edf805914c61f92
Conner Martin <Conner.Martin@mindbodyonline.com>
2019-12-26 21:58:59 -0800

    Added grabbing genres and adding them to cache for quicker frontend experience

--- SpotAPIFrontEnd/Controllers/HomeController.cs
+++ SpotAPIFrontEnd/Controllers/HomeController.cs
@@ -12,16 +12,20 @@ using System.Text.Json;
 using Microsoft.Extensions.Configuration;
 using System.Net.Http;
 using SpotAPIFrontEnd.Services;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace SpotAPIFrontEnd.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IMemoryCache _cache;
         private readonly ILogger<HomeController> _logger;
         private readonly IConfiguration _config;
         private readonly SpotApiService _sas;
-        public HomeController(ILogger<HomeController> logger, IConfiguration config, SpotApiService sas)
+        public HomeController(IMemoryCache cache, ILogger<HomeController> logger, IConfiguration config, SpotApiService sas)
         {
+            _cache = cache;
             _logger = logger;
             _config = config;
             _sas = sas;
@@ -29,7 +33,6 @@ namespace SpotAPIFrontEnd.Controllers
 
         public IActionResult Index()
         {     
-
             return View();
                         
         }
@@ -44,7 +47,8 @@ namespace SpotAPIFrontEnd.Controllers
                 baseUrl += "&response_type=code";
                 baseUrl += "&redirect_uri=" + _config.GetValue<string>("redirectUri");
                 baseUrl += "&scope=playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private user-read-email";
-                response = await client.GetAsync(baseUrl);                
+                var baseUri = new Uri(baseUrl);
+                response = await client.GetAsync(baseUri).ConfigureAwait(true);                
             }
             return Redirect(response.RequestMessage.RequestUri.ToString());
         }
@@ -58,7 +62,7 @@ namespace SpotAPIFrontEnd.Controllers
                 var responseContent ="";
                 using (var client = new HttpClient())
                 {
-                    var baseUrl = "https://accounts.spotify.com/api/token";
+                    var baseUrl = new Uri("https://accounts.spotify.com/api/token");
                     var body = new Dictionary<string, string>()
                     {
                         { "grant_type","authorization_code" },
@@ -67,11 +71,14 @@ namespace SpotAPIFrontEnd.Controllers
                         {"client_id",  _config.GetValue<string>("clientId")},
                         {"client_secret",  _config.GetValue<string>("clientSecret") }
                     };
-                    response = await client.PostAsync(baseUrl,new FormUrlEncodedContent(body));
-                    responseContent = response.Content.ReadAsStringAsync().Result.ToString();                    
+                    response = await client.PostAsync(baseUrl,new FormUrlEncodedContent(body)).ConfigureAwait(true);
+                    responseContent = response.Content.ReadAsStringAsync().Result;                    
                 }
                 var tokenCookie = JsonSerializer.Deserialize<Token>(responseContent);
                 HttpContext.Response.Cookies.Append("spotauthtoke", tokenCookie.access_token);
+
+                //why not set genres now?
+                var setGenres = await GenresToArray(tokenCookie.access_token).ConfigureAwait(true);
             }
 
             return RedirectToAction("Index");
@@ -85,7 +92,7 @@ namespace SpotAPIFrontEnd.Controllers
             //jsonify params
             var jsonParams = JsonSerializer.Serialize(spotParams);
             //send to spot api
-            var res = await _sas.Access("post", auth, "/Create", jsonParams);
+            var res = await _sas.Access("post", auth, "/Create", jsonParams).ConfigureAwait(true);
             //returns okay response with a redirect to viewing the tracks?
             return new JsonResult(new CreatePlaylistRequest { });
         }
@@ -94,6 +101,32 @@ namespace SpotAPIFrontEnd.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<string[]> GenresToArray(string auth)
+        {
+            var spotifyGenres = new GenreResponse();
+            if (!_cache.TryGetValue("spotifyGenres", out spotifyGenres))
+            {
+                var baseAddress = "https://api.spotify.com/v1/recommendations/available-genre-seeds";
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Add("Authorization", "Bearer " + auth);
+
+                    client.BaseAddress = new Uri(baseAddress);
+                    var genreResponse = await client.GetAsync(baseAddress);
+                    spotifyGenres = JsonSerializer.Deserialize<GenreResponse>(await genreResponse.Content.ReadAsByteArrayAsync(), null);
+
+                }
+
+
+                _cache.Set("spotifyGenres", spotifyGenres);
+
+            }
+
+
+
+            return spotifyGenres.genres;
         }
     }
 }
---
+++ SpotAPIFrontEnd/Models/GenreResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpotAPIFrontEnd.Models
+{
+    public class GenreResponse
+    {
+        public string[] genres { get; set; }
+    }
+}
